c9664cbf9ad95bfaf5f2d719ef133ac2f061bc8e
Sarah Proctor <sproctor950@gmail.com>
2023-03-20 15:55:43 -0700

    :recycle: moves to functional components

--- bugbot/src/App.js
+++ bugbot/src/App.js
@@ -1,41 +1,27 @@
-import React, { Component } from 'react'
-import BugBot from './components/BugBot'
-import bug from './assets/bug.jpeg'
-import './App.css'
+import React, { useState } from "react"
+import BugBot from "./components/BugBot"
+import bug from "./assets/bug.jpeg"
+import "./App.css"
 
-class App extends Component{
-  constructor(props){
-    super(props)
-    this.state = {
-      userInput: "",
-    }
+const App = () => {
+  const [userInput, setUserInput] = useState("")
+
+  const robot = (e) => {
+    setUserInput(e.target.value)
   }
 
-  robot = (e) => {
-    this.setState({userInput: e.target.value})
-  }
-
-  render(){
-    let {userInput} = this.state
-    return(
-      <div>
-        <h1>Bug Bot</h1>
-        <img
-          src={bug}
-          alt="junebug clipart"
-        />
-        <h3>Enter text here:</h3>
-        <input
-          onChange={this.robot}
-          value={userInput}
-        />
-        <br />
-        <div className="outcome">
-          <BugBot userInput={userInput} />
-        </div>
+  return (
+    <>
+      <h1>Bug Bot</h1>
+      <img src={bug} alt="junebug clipart" />
+      <h3>Enter text here:</h3>
+      <input onChange={robot} value={userInput} />
+      <br />
+      <div className="outcome">
+        <BugBot userInput={userInput} />
       </div>
-    )
-  }
+    </>
+  )
 }
 
 export default App
--- bugbot/src/components/BugBot.js
+++ bugbot/src/components/BugBot.js
@@ -1,18 +1,21 @@
-import React, { Component } from 'react'
+import React from "react"
 
-class BugBot extends Component {
-
-  bugFeature = (input) => {
-    let response = ["it", "is", "not", "a", "bug", "it", "is", "a", "feature"]
+const BugBot = ({ userInput }) => {
+  const bugFeature = (input) => {
+    let response = [
+      "it ",
+      "is ",
+      "not ",
+      "a ",
+      "bug ",
+      "it ",
+      "is ",
+      "a ",
+      "feature "
+    ]
+    return input
   }
 
-  render() {
-    const {userInput} = this.props
-    return (
-      <>
-        <h4>{this.bugFeature(userInput)}</h4>
-      </>
-    )
-  }
+  return <h4>{bugFeature(userInput)}</h4>
 }
 export default BugBot
